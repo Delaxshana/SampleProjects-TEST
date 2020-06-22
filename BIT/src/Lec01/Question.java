5d0736a65ca4bdb65df523d02af65978ffadf4d3
Delaxshana Santhiravathanan <48511708+Delaxshana@users.noreply.github.com>
2020-06-22 22:37:10 +0530

    BIT

---
+++ BIT/src/Lec01/Question.java
@@ -0,0 +1,13 @@
+package Lec01;
+
+public class Question {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("************************");
+		System.out.println("The\tOpenArc\tCampus");
+		System.out.println("************************");
+
+	}
+
+}
